f7d44840d23307232ca8700d1fca015d590f7605
Poy Chang <poypost@gmail.com>
2024-03-29 09:23:35 +0800

    first commit

---
+++ HttpClientSide/Program.cs
@@ -0,0 +1,103 @@
+﻿/*
+ * 可以使用以下命令查看端口占用情况
+ * netstat -na | find "7016"
+ * 
+ * 或開啟 PowerShell 執行以下指令來持續觀察（每秒更新一次）
+ * while ($true) { cls; Write-Output "Current: $(Get-Date)"; netstat -na | Select-String "7016"; Start-Sleep -Seconds 1 }
+ */
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateDefaultBuilder()
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddHttpClient();
+        services.AddHostedService<Startup>();
+    });
+var host = builder.Build();
+host.Run();
+
+class Startup(IHttpClientFactory httpClientFactory, HttpClient httpClient) : IHostedService
+{
+    private readonly IHttpClientFactory _httpClientFactory = httpClientFactory;
+    private readonly HttpClient _httpClient = httpClient;
+    const string url = "https://localhost:7016/WeatherForecast";
+    const int count = 100;
+    private static HttpClient _staticHttpClient = new HttpClient();
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        // Version 1: new a HttpClient instance every time
+        //TestHttpClientVersion1(url);
+        // Version 2: use using statement to dispose HttpClient instance
+        //TestHttpClientVersion2(url);
+        // Version 3: use static HttpClient and reuse it
+        //TestHttpClientVersion3(url);
+        // Version 4: inject HttpClient instance and reuse it
+        //TestHttpClientVersion4(_httpClient,url);
+        // Version 5: inject HttpClientFactory and create HttpClient instance by it
+        //TestHttpClientVersion5(_httpClientFactory, url);
+
+        return Task.CompletedTask;
+
+        void TestHttpClientVersion1(string url)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                var httpClient = new HttpClient();
+                httpClient.GetAsync(url).GetAwaiter().GetResult();
+                Console.WriteLine(i);
+                Thread.Sleep(0_100);
+            }
+        }
+
+        void TestHttpClientVersion2(string url)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                using var httpClient = new HttpClient();
+                httpClient.GetAsync(url).GetAwaiter().GetResult();
+                Console.WriteLine(i);
+                Thread.Sleep(0_100);
+            }
+        }
+
+        void TestHttpClientVersion3(string url)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                _staticHttpClient.GetAsync(url).GetAwaiter().GetResult();
+                Console.WriteLine(i);
+                Thread.Sleep(0_100);
+            }
+        }
+
+        void TestHttpClientVersion4(HttpClient httpClient, string url)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                httpClient.GetAsync(url).GetAwaiter().GetResult().Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                Console.WriteLine(i);
+                Thread.Sleep(200_000);
+            }
+        }
+
+        void TestHttpClientVersion5(IHttpClientFactory httpClientFactory, string url)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                httpClientFactory.CreateClient()
+                    .GetAsync(url).GetAwaiter().GetResult()
+                    .Content
+                    .ReadAsStringAsync().GetAwaiter().GetResult();
+                Console.WriteLine(i);
+                Thread.Sleep(0_100);
+            }
+        }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ README.md
@@ -0,0 +1,36 @@
+# 測試 HttpClient Socket 使用情況
+
+要在 Windows 中查看當前 Socket 的使用狀況，可以使用以下命令查看端口占用情况：
+
+```cmd
+netstat -na | find "7016"
+```
+
+或開啟 PowerShell 執行以下指令來持續觀察（每秒更新一次）：
+
+```powershell
+while ($true) { cls; Write-Output "Current: $(Get-Date)"; netstat -na | Select-String "7016"; Start-Sleep -Seconds 1 }
+```
+
+## 測試
+
+此範例測試以下使用情況：
+
+1. 每次呼叫都建立新的 HttpClient 實體
+2. 每次呼叫都透過 using 方式來建立 HttpClient 實體，使用結束後會 dispose
+3. 每次呼叫都使用靜態建立的 HttpClient 實體，並重複用
+4. 使用相依性注入的方式使用 HttpClient
+5. 使用相依性注入的方式使用  HttpClientFactory
+
+## netstat
+
+`netstat` 指令可以用來查詢各種網路相關資訊，檢測各種網路相關的問題，可以列出非常多很有用的資訊，像 socket、TCP、UDP、IP 與 ethernet 層的各種資訊都可以利用 `netstat` 來查詢
+
+此指令所輸出的 `state` 欄位，欄位指的是 TCP 連接的當前狀態，以下為常見的狀態：
+
+- `LISTENING` 指的是伺服器上的端口正在聽取來自任何客戶端的連接請求
+- `ESTABLISHED` 表示一個連接已經成功建立，數據可以在兩個方向上傳輸
+- `TIME_WAIT` 本地端已經確認了遠端的終止請求，但是保持連接一段時間以確保遠端收到了最終的確認
+- `CLOSED` 連接已經完全終止，不再使用
+
+其中當 socket 處於 `TIME_WAIT` 狀態時，該 socket 的端口號是不能立即被其他新的連接使用的。`TIME_WAIT` 狀態存在的主要原因是為了確保在同一個連接的**舊**數據包在網絡中完全消失，這樣它們就不會被誤解為新連接的數據包。這是 TCP 協議的一部分，旨在確保可靠性和數據完整性。
